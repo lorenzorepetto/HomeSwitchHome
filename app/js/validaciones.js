a5e64cebc457d827fb4d3629464cf4ee3d607633
onofrimelisa <mel.onofri@gmail.com>
2019-04-19 22:57:17 -0300

    validaciones registrar_residencia

--- app/js/validaciones.js
+++ app/js/validaciones.js
@@ -21,7 +21,7 @@ function validarIniciar(){
       document.getElementById("msjPassword2").style.display = "block";
       return false;
    }
-
+   return true;
 }
 
 function validarRegistrar(){
@@ -32,13 +32,15 @@ function validarRegistrar(){
   var apellido = document.getElementById("apellido").value;
   var telefono = document.getElementById("telefono").value;
   var fecha_nacimiento = document.getElementById("fecha_nacimiento").value;
-  //var foto = document.getElementById("foto").value;
   var marca_tarjeta = document.getElementById("marca_tarjeta").value;
   var numero_tarjeta = document.getElementById("numero_tarjeta").value;
   var titular_tarjeta = document.getElementById("titular_tarjeta").value;
-  var fecha_vencimiento_tarjeta = document.getElementById("nombre").value;
+  var fecha_vencimiento_tarjeta = document.getElementById("fecha_vencimiento_tarjeta").value;
 
-  if (campoVacio(email) || campoVacio(pass) || campoVacio(nombre) || campoVacio(apellido) || campoVacio(telefono) || campoVacio(fecha_nacimiento) || campoVacio(marca_tarjeta) || campoVacio(numero_tarjeta) || campoVacio(titular_tarjeta) || campoVacio(fecha_vencimiento_tarjeta)){
+  if (campoVacio(email) || campoVacio(pass) || campoVacio(nombre) || campoVacio(apellido) || campoVacio(telefono) 
+    || campoVacio(fecha_nacimiento) || campoVacio(marca_tarjeta) || campoVacio(numero_tarjeta) || campoVacio(titular_tarjeta) || 
+    campoVacio(fecha_vencimiento_tarjeta)){
+
     document.getElementById("msjEmail").style.display = "block";
     document.getElementById("msjNombre").style.display = "block";
     document.getElementById("msjPassword1").style.display = "block";
@@ -51,10 +53,37 @@ function validarRegistrar(){
     document.getElementById("msjNumero").style.display = "block";
     return false;
   }
+  return true;
 
 }
 
 
 function validarAgregarResidencia(){
+
+  var nombre = document.getElementById("nombre").value;
+  var descripcion = document.getElementById("descripcion").value;
+  var calle = document.getElementById("calle").value;
+  var altura = document.getElementById("altura").value;
+  var ciudad = document.getElementById("ciudad").value
+  var provincia = document.getElementById("provincia").value;
+  var capacidad = document.getElementById("capacidad").value;
+  var foto = document.getElementById("foto").value;
+
+  if (campoVacio(nombre) || campoVacio(descripcion)|| campoVacio(calle)|| campoVacio(altura)
+    || campoVacio(ciudad)|| campoVacio(provincia)|| campoVacio(capacidad)
+    || campoVacio(foto)){
+    document.getElementById("msjNombreR").style.display = "block";
+  document.getElementById("msjDescripcionR").style.display = "block";
+  document.getElementById("msjCalleR").style.display = "block";
+  document.getElementById("msjAlturaR").style.display = "block";
+  document.getElementById("msjCiudadR").style.display = "block";
+  document.getElementById("msjProvinciaR").style.display = "block";
+  document.getElementById("msjCapacidadR").style.display = "block";
+  document.getElementById("msjFotoR").style.display = "block";
+  return false;
+
+  }
+
+
   return true;
 }
