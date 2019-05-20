55bae1c0aa651fb94bcf56f39e805e50ad31848b
lorenzorepetto <lorenzorepetto28@gmail.com>
2019-05-19 21:34:09 -0300

    me olvide la db

--- homeSwitchHome.sql
+++ homeSwitchHome.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-04-2019 a las 00:43:38
+-- Tiempo de generación: 20-05-2019 a las 02:33:17
 -- Versión del servidor: 10.1.38-MariaDB
 -- Versión de PHP: 7.3.2
 
@@ -38,6 +38,16 @@ CREATE TABLE `estadias` (
   `estado_logico` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
+--
+-- Volcado de datos para la tabla `estadias`
+--
+
+INSERT INTO `estadias` (`id`, `caducada`, `id_residencia`, `semana`, `ocupada`, `monto`, `estado_logico`) VALUES
+(1, 0, 1, 100, 0, 1000, 0),
+(2, 0, 1, 21, 0, 15000, 0),
+(3, 0, 1, 41, 0, 2000, 0),
+(4, 0, 2, 19, 0, 10000, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -51,6 +61,32 @@ CREATE TABLE `hotsales` (
   `estado` tinyint(1) NOT NULL,
   `porcentaje` float NOT NULL,
   `id_estadia` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pujas`
+--
+
+CREATE TABLE `pujas` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_usuario` int(10) UNSIGNED NOT NULL,
+  `id_subasta` int(10) UNSIGNED NOT NULL,
+  `monto_apostado` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reservas`
+--
+
+CREATE TABLE `reservas` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `id_usuario` int(10) UNSIGNED NOT NULL,
+  `id_estadia` int(10) UNSIGNED NOT NULL,
+  `monto_pagado` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
@@ -77,8 +113,8 @@ CREATE TABLE `residencias` (
 --
 
 INSERT INTO `residencias` (`id`, `nombre`, `descripcion`, `foto`, `capacidad`, `estado_logico`, `calle`, `altura`, `ciudad`, `provincia`) VALUES
-(1, 'Aldea Andina', 'Los servicios de Aldea Andina incluyen Wi-Fi en todo el recinto, zonas comunes y privadas, piscina climatizada interior y exterior, servicios de SPA, cancha de tenis, un \"quincho para asados\", un fogón, lugar de juegos de mesa y un parque de juegos para los niños en el fabuloso parque de cuatro acres rodeado de una gran variedad de árboles. ', 'app/img/residencias/residencia1.jpg', 10, 0, 'Avenida de Los Pioneros', 1400, 'San Carlos de Bariloche', 'Río Negro'),
-(2, 'Building Albar', 'El complejo Building Albar tiene dos espacios diferentes: un área gastronómica abierta a todo el público y un área de recreación privada con piscinas, decks y solárium. Tiene cocheras con portón automatizado con comando a distancia. Ademas cuentan con sauna, hidromasaje, piscina climatizada, área de fitness y vestuarios con acceso independiente. El complejo brinda todo en cuanto a tecnología, confort, seguridad y un diseño moderno en armonía con el entorno. ', 'app/img/residencias/residencia2.jpg', 6, 0, 'Av. Bunge ', 1867, 'Pinamar', 'Buenos Aires'),
+(1, 'Aldea Andina', 'Los servicios de Aldea Andina incluyen Wi-Fi en todo el recinto, zonas comunes y privadas, piscina climatizada interior y exterior, servicios de SPA, cancha de tenis, un \"quincho para asados\", un fogón, lugar de juegos de mesa y un parque de juegos para los niños en el fabuloso parque de cuatro acres rodeado de una gran variedad de árboles. ', 'app/img/residencias/residencia1.jpg', 10, 0, 'Avenida de Los Pioneros', 1403, 'San Carlos de Bariloche', 'Río Negro'),
+(2, 'Building Albar', 'El complejo Building Albar tiene dos espacios diferentes: un área gastronómica abierta a todo el público y un área de recreación privada con piscinas, decks y solárium. Tiene cocheras con portón automatizado con comando a distancia. Ademas cuentan con sauna, hidromasaje, piscina climatizada, área de fitness y vestuarios con acceso independiente. El complejo brinda todo en cuanto a tecnología, confort, seguridad y un diseño moderno en armonía con el entorno. ', 'app/img/residencias/residencia2.jpg', 6, 0, 'Av. Bunge ', 1868, 'Pinamar', 'Buenos Aires'),
 (3, 'Apartur Mar Del Plata', 'El desarrollo cuenta con 1 piscina descubierta, bar - cafetería, cancha de tenis, de boley y de futbol, cuarto de juegos , área de recreación, etc. Estacionamiento para todas las unidades.\r\n', 'app/img/residencias/residencia3.jpg', 10, 0, 'Jorge Hernandez', 1066, 'Mar Del Plata', 'Buenos Aires'),
 (4, 'Club Cariló Playa', 'El complejo cuenta con un clubhouse, una sala para leer, un solarium, piscina exterior y cubierta, gimnasio, sauna, acceso directo a la playa y WI-FI. El condominio está formado por 4 edificios con departamentos de 1, 2 y 3 ambientes totalmente equipados con parrillas y balcones individuales. Dichos departamentos tienen una capacidad de 2 a 7 personas, con vista al mar y al bosque. Desarrollados en un amplio parque con salida exclusiva a la playa.             ', 'app/img/residencias/residencia4.jpg', 15, 0, 'Avutarda', 2909, 'Cariló', 'Buenos Aires');
 
@@ -94,8 +130,17 @@ CREATE TABLE `subastas` (
   `fecha_fin` date NOT NULL,
   `estado` tinyint(1) NOT NULL,
   `usuario_ganador` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `id_estadia` int(10) UNSIGNED NOT NULL
+  `id_estadia` int(10) UNSIGNED NOT NULL,
+  `monto` int(10) NOT NULL,
+  `monto_actual` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `subastas`
+--
+
+INSERT INTO `subastas` (`id`, `fecha_inicio`, `fecha_fin`, `estado`, `usuario_ganador`, `id_estadia`, `monto`, `monto_actual`) VALUES
+(7, '0000-00-00', '0000-00-00', 0, ' ', 2, 5000, 5000);
 
 -- --------------------------------------------------------
 
@@ -135,32 +180,6 @@ INSERT INTO `usuarios` (`id`, `email`, `password`, `nombre`, `apellido`, `fecha_
 (6, 'jorge@gmail.com', 'jorge1234', 'Jorge', 'Paulos', '1980-04-13', 'app/img/usuarios/jorge.jpg', '0113751314', 2, 'MASTERCARD', '8254435411', '', '04/2022', 'ESTANDAR', 0, NULL),
 (7, 'julia@gmail.com', 'julia1234', 'Julia', 'Rosales', '1990-09-04', 'app/img/usuarios/julia.jpg', '0113441586', 2, 'VISA', '512497543', '', '04/2022', 'ESTANDAR', 0, NULL);
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios_estadias`
---
-
-CREATE TABLE `usuarios_estadias` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `id_usuario` int(10) UNSIGNED NOT NULL,
-  `id_estadia` int(10) UNSIGNED NOT NULL,
-  `monto_pagado` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios_subastas`
---
-
-CREATE TABLE `usuarios_subastas` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `id_usuario` int(10) UNSIGNED NOT NULL,
-  `id_subastas` int(10) UNSIGNED NOT NULL,
-  `monto_apostado` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
 --
 -- Índices para tablas volcadas
 --
@@ -175,6 +194,18 @@ ALTER TABLE `estadias`
 -- Indices de la tabla `hotsales`
 --
 ALTER TABLE `hotsales`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `pujas`
+--
+ALTER TABLE `pujas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `reservas`
+--
+ALTER TABLE `reservas`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -197,18 +228,6 @@ ALTER TABLE `usuarios`
   ADD UNIQUE KEY `mail` (`email`);
 
 --
--- Indices de la tabla `usuarios_estadias`
---
-ALTER TABLE `usuarios_estadias`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `usuarios_subastas`
---
-ALTER TABLE `usuarios_subastas`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -216,12 +235,24 @@ ALTER TABLE `usuarios_subastas`
 -- AUTO_INCREMENT de la tabla `estadias`
 --
 ALTER TABLE `estadias`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `hotsales`
 --
 ALTER TABLE `hotsales`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pujas`
+--
+ALTER TABLE `pujas`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `reservas`
+--
+ALTER TABLE `reservas`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
@@ -234,25 +265,13 @@ ALTER TABLE `residencias`
 -- AUTO_INCREMENT de la tabla `subastas`
 --
 ALTER TABLE `subastas`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT de la tabla `usuarios_estadias`
---
-ALTER TABLE `usuarios_estadias`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `usuarios_subastas`
---
-ALTER TABLE `usuarios_subastas`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
